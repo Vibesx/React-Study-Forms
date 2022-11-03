7f5d44625822842d5735b85c608bd7eebac0a3f0
vibesx <lucian.paslariu@gmail.com>
2022-11-03 15:06:32 +0200

    finished using useReducer but did so on the wrong branch; not going back to change everything, got the main idea

--- src/hooks/use-input.js
+++ src/hooks/use-input.js
@@ -1,27 +1,52 @@
-import { useState } from "react";
+import { useReducer } from "react";
+
+const initialInputState = {
+	value: "",
+	isTouched: false,
+};
+
+const inputStateReducer = (state, action) => {
+	if (action.type === "INPUT") {
+		return {
+			value: action.value,
+			isTouched: state.isTouched,
+		};
+	}
+	if (action.type === "BLUR") {
+		return { value: state.value, isTouched: true };
+	}
+	if (action.type === "RESET") {
+		return initialInputState;
+	}
+	return initialInputState;
+};
 
 const useInput = (validateValue) => {
-	const [enteredValue, setEnteredValue] = useState("");
-	const [isTouched, setIsTouched] = useState(false);
+	// const [enteredValue, setEnteredValue] = useState("");
+	// const [isTouched, setIsTouched] = useState(false);
 
-	const valueIsValid = validateValue(enteredValue);
-	const hasError = !valueIsValid && isTouched;
+	const [inputState, dispatch] = useReducer(
+		inputStateReducer,
+		initialInputState
+	);
+
+	const valueIsValid = validateValue(inputState.value);
+	const hasError = !valueIsValid && inputState.isTouched;
 
 	const valueChangeHandler = (event) => {
-		setEnteredValue(event.target.value);
+		dispatch({ input: "INPUT", value: event.target.value });
 	};
 
 	const inputBlurHandler = (event) => {
-		setIsTouched(true);
+		dispatch({ input: "BLUR" });
 	};
 
 	const reset = () => {
-		setEnteredValue("");
-		setIsTouched(false);
+		dispatch({ input: "RESET" });
 	};
 
 	return {
-		value: enteredValue,
+		value: inputState.value,
 		isvalid: valueIsValid,
 		hasError,
 		valueChangeHandler,
